a081831c2805d5271e9bf10e6e9ecf7c118495b9
Nadia Romagnoni <60789454+Ginevra7@users.noreply.github.com>
2021-03-31 19:03:55 +0200

    Add files via upload
    fiche ville php  complet  
    style css pa complète

--- fiche_ville.php
+++ fiche_ville.php
@@ -1,21 +1,29 @@
 <?php 
 	session_start();
 ?>
+
     <html>
     <head classe = "header">
 	<title>Fiche ville</title>
     <link rel="stylesheet" href="styleH.css" type="text/css" media="screen" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <style type="text/css">
-		.tde {
+		.tde{
 			height:20px;
 			width:20px;
 			cursor:pointer;
 			}
 	    #glob {display: flex;}
 		table{
-			margin-top:50px;
+			margin-top:0px;
 		}
+		#graph{
+			display:block;
+			margin:auto;
+			position:relative;		
+			top:500px;	
+		}
+		
 	
 	</style>   
 
@@ -25,8 +33,8 @@
 		  
         <?php include ("head2.php");?>	
 		<?php
-			//$nom_Ville= $_GET["Ville"];
-			$Ville="Montpellier";
+			$Ville= $_GET["Ville"];
+			//$Ville="Paris";
 			include('bd.php'); 
 			$bdd = getBD();	
 			$rep=$bdd->query('select * from villes where Ville=\''.$Ville.'\''); 
@@ -36,7 +44,7 @@
 		<h2>		
 			<?php echo $ligne["Ville"]; ?>
 		</h2>
-		
+		 
 		<img id="image" style="display:block;margin:auto;margin-top:50px;" src= <?php echo  $ligne["url_photo"];?> alt="Image ville"  /> 
 		
         <div class="tableau">
@@ -46,65 +54,35 @@
 					<th colspan = "6" >   Voici la fiche de <?php echo $ligne["Ville"]; ?> </th
 		   		</tr>
 		   		</thead>   				
-					<?php
-						echo"<tr>";
-						echo"<td>";
-						echo "Culture";
-						echo"</td>";
-						echo"<td>";
-						echo "Sport";
-						echo"</td>";
-						echo"<td>";
-						echo "Loyer";
-						echo"</td>";
-						echo"<td>";
-						echo "Météo";
-						echo"</td>";
-						echo"<td>";
-						echo "Crous";
-						echo"</td>";
-						echo"<td>";
-						echo "Sécurité";
-						echo"</tr>";
-					?>
+					
 		<?php
-			$rep1=$bdd->query('select Prop_dEtudiant from villes'); 
-			$ligne1=$rep1->fetch();
-			$rep2=$bdd->query('select * from culture where Ville=\''.$Ville.'\''); 
-			$ligne2=$rep2->fetch();
-			$tot=$ligne2['nbCinema']+$ligne2['nbFestival']+$ligne2['nbBiblio']+$ligne2['nbMusee'];	
-			$tot1=($tot/$ligne1['Prop_dEtudiant'])*100;
-			echo"<tr>";
-			echo"<td>";
-			echo $tot1;
-			echo"</td>";
-			$rep2=$bdd->query('select * from sport where Ville=\''.$Ville.'\''); 
-			$ligne2=$rep2->fetch();
-			$tot=$ligne2['nbEquipements']+$ligne2['nbInstallations'];	
-			$tot2=($tot/$ligne1['Prop_dEtudiant'])*10;
-			echo"<td>";
-			echo $tot2;
-			echo"</td>";
-			$rep2=$bdd->query('select * from crous where Ville=\''.$Ville.'\''); 
-			$ligne2=$rep2->fetch();
-			$tot=$ligne2['Restaurant'];	
-			$tot3=($tot/$ligne1['Prop_dEtudiant'])*1000;
-			echo"<td>";
-			echo"</td>";
-			echo"<td>";
-			echo"</td>";
-			echo"<td>";
-			echo $tot3;
-			echo"</td>";
-			echo"<td>";
-			echo"</tr>";
-			$value1=(($tot1+$tot2+$tot3)/3)*100;
-			
+			include 'note_culture.php';
+			include 'note_sport.php';
+			include 'note_crous.php';
+			include 'note_meteo.php';
+			include 'note_transport.php';
+			include  'note_securite.php';
+			include  'note_loyer.php';
+			$culture=noteCulture($Ville);
+			$sport=noteSport($Ville);
+			$crous=noteCrous($Ville);
+			$meteo=noteMeteo($Ville);
+		
+			$transport=noteTransport($Ville);
+			$securite=noteSecurite($Ville);
+			$loyer=noteLoyer($Ville);
+			$value1=(($culture+$sport+$crous+$meteo+$transport+$securite+$loyer)/7)*10;
 		?>
-								
+	
+		<?php
+			include 'graph_ficheVille.php';
+			GenerateGraph($culture,$sport,$crous,$meteo,$transport,$securite,$loyer);
+		?>	
+		<img id="graph" src="graph_ficheVille.jpg" />	
+		
     	</table>
 		</div>
-		<div style="display:block;margin:auto;margin-top:50px;margin-top:-230px;width:100px;">
+		<div style="display:block;margin:auto;margin-top:-200px;margin-bottom:-200px;width:100px;">
 			<div style="height:20px; 
 			width: <?=$value1;?>px; 
 			background:#E0E001;" >
@@ -116,7 +94,7 @@
 					<img id="tde_3" src="star.png" alt="image etoiles" class="tde"/>
 					<img id="tde_4" src="star.png" alt="image etoiles" class="tde"/>
 					<img id="tde_5" src="star.png" alt="image etoiles" class="tde"/>
-					<?php $value1=(($tot1+$tot2+$tot3)/3)*100; ?>
+		
 					
 				</div>
 			</div>
@@ -124,7 +102,7 @@
 		
        
 			
-		<?php //include ("foot2.php");?>
+		<?php// include ("foot2.php");?>
         </body>
     </html>
    
